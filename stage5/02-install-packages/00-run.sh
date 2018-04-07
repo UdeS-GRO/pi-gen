f9fabb0c4984879964fbf4ec6fabdad3756c3095
François Ferland <git@francoisferland.com>
2018-04-07 11:30:07 -0400

    Working Arduino 1.8.5 install (in /opt/arduino).

--- stage5/02-install-packages/00-run.sh
+++ stage5/02-install-packages/00-run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -e
 
 # Manual install of Arduino IDE 1.8.5
-install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/tmp/arduino-1.8.5"
-tar xf files/arduino-1.8.5-linuxarm.tar.xz -C "${ROOTFS_DIR}/tmp/arduino-1.8.5"
+install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/opt/arduino"
+install -v -o 1000 -g 1000 files/arduino-1.8.5-linuxarm.tar.xz "${ROOTFS_DIR}/opt/arduino/"
 on_chroot << EOF
-cd /tmp/arduino-1.8.5
+cd /opt/arduino
+tar xf arduino-1.8.5-linuxarm.tar.xz 
+cd arduino-1.8.5
 ./install.sh
-rm -rf /tmp/arduino-1.8.5
+cd ..
 EOF
 
