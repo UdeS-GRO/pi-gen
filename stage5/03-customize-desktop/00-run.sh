87bf6e4173cb5819babbbf4461d1c60ad9852e21
François Ferland <git@francoisferland.com>
2018-05-02 16:35:55 -0400

    Working generated wallpaper.

--- stage5/03-customize-desktop/00-run.sh
+++ stage5/03-customize-desktop/00-run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -e
 
-# Annotate the UdeS logo with the current date (with ImageMagick)
-convert files/udes-logo.png label:"Généré: `date`" -append files/udes-bg.png
 
 # Copy the wallpaper image, then set it through the file manager config app.
-install -v -o 1000 -g 1000 files/udes-bg.png "${ROOTFS_DIR}/usr/share/s3gro/"
+install -v files/udes-logo.png "${ROOTFS_DIR}/tmp/"
+mkdir -p ${ROOTFS_DIR}/home/pi/.config/pcmanfm/LXDE-pi/
+install -v -o 1000 -g 1000 files/desktop-items-0.conf ${ROOTFS_DIR}/home/pi/.config/pcmanfm/LXDE-pi/
 
 on_chroot << EOF
-su pi
-pcmanfm --set-wallpaper "/usr/share/s3gro/udes-bg.png"
-pcmanfm --wallpaper-mode center
+# Annotate the UdeS logo with the current date (with ImageMagick)
+mkdir -p /usr/share/s3gro/
+convert /tmp/udes-logo.png label:"Généré: `date`" -append /usr/share/s3gro/udes-bg.png
+rm /tmp/udes-logo.png
 EOF
