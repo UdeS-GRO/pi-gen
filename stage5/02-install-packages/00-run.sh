239afb02ca5102d77c8fe0c299814a6e60adf9f5
François Ferland <git@francoisferland.com>
2019-05-14 15:30:30 -0400

    Adding qtcharts.

--- stage5/02-install-packages/00-run.sh
+++ stage5/02-install-packages/00-run.sh
@@ -3,13 +3,22 @@
 # Manual install of Arduino IDE 1.8.5
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/opt/arduino"
 install -v -o 1000 -g 1000 files/arduino-1.8.5-linuxarm.tar.xz "${ROOTFS_DIR}/opt/arduino/"
+install -v -o 1000 -g 1000 files/qtcharts.deb "${ROOTFS_DIR}/tmp/"
 on_chroot << EOF
+
+dpkg -i /tmp/qtcharts.deb
+
+apt-get -y remove python-pip python3-pip
+
+wget https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+pip install -U platformio
+
 cd /opt/arduino
 tar xf arduino-1.8.5-linuxarm.tar.xz 
 cd arduino-1.8.5
 ./install.sh
 cd ..
 
-pip install -U platformio
 EOF
 
